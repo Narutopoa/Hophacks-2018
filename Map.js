06116df782f108ab066c6aa934bc0a30752d7647
Mike O <mikeoluwole@gmail.com>
2018-02-17 11:32:12 -0500

    LAnding page added

--- Map.js
+++ Map.js
@@ -2,8 +2,19 @@ function initMap() {
         var uluru = {lat: -25.363, lng: 131.044};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
-          center: uluru
+          center: uluru,
+          mapTypeId: 'terrain'
         });
+        // Create a <script> tag and set the USGS URL as the source.
+        var script = document.createElement('script');
+        // This example uses a local copy of the GeoJSON stored at
+        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+        script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+        document.getElementsByTagName('head')[0].appendChild(script);
+        var marker = new google.maps.Marker({
+            position: map.getCenter(),
+            map: map
+            });
         infoWindow = new google.maps.InfoWindow;
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
@@ -13,8 +24,8 @@ function initMap() {
             };
 
             infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
+            //infoWindow.setContent('Location found.');
+            //infoWindow.open(map);
             map.setCenter(pos);
             var marker = new google.maps.Marker({
             position: map.getCenter(),
@@ -27,6 +38,18 @@ function initMap() {
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
         }
+        // Loop through the results array and place a marker for each
+        // set of coordinates.
+        window.eqfeed_callback = function(results) {
+          for (var i = 0; i < results.features.length; i++) {
+            var coords = results.features[i].geometry.coordinates;
+            var latLng = new google.maps.LatLng(coords[1],coords[0]);
+            var marker = new google.maps.Marker({
+              position: latLng,
+              map: map
+            });
+        }
+}
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
