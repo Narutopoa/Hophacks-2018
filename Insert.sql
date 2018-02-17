5e4c483d9cabab67991d7d852648dd3582998289
aleex-adal <aleex.adal@gmail.com>
2018-02-17 17:22:50 -0500

    Fixed sample php

--- Insert.sql
+++ Insert.sql
@@ -1,9 +1,9 @@
 /*Find the dates and exchanges where that exchange has more than x trades per minute, while the exchange specific Bitcoin price has been below y.*/
 delimiter //
 DROP PROCEDURE IF EXISTS InsertOrg//
-CREATE PROCEDURE InsertOrg(IN Name varchar(30), Street varchar(50), City varchar(50), State	char(2), Related_Major1	varchar(50),Org_Type varchar(20))
+CREATE PROCEDURE InsertOrg(IN nm varchar(30), strt varchar(50), city varchar(50), state	char(2), relmaj1 varchar(50), orgtype varchar(20))
 BEGIN
 	INSERT INTO Organizations (Name, Street, City, State, Related_Major1, Org_Type)
-	VALUES (Name, Street, City, State, Related_Major1, Org_Type);
+	VALUES (nm, strt, city, state, relmaj1, orgtype);
 END//
 delimiter ;
